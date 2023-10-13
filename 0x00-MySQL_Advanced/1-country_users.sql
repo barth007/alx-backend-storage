172511155a50aa0b3d58376292f15693407ab3ff
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-10-13 06:23:21 +0000

    added task 1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,28 @@
--- This scripts create a table called users
+-- This script creates a table called users
 
-CREATE TABLE IF NOT EXISTS users (
+DELIMITER $$
+DROP PROCEDURE IF EXISTS create_detail $$
+
+CREATE PROCEDURE create_detail()
+BEGIN
+  -- DECLARE id INT;
+  -- DECLARE email VARCHAR(255);
+  -- DECLARE name VARCHAR(255);
+  -- DECLARE country ENUM;
+
+
+  -- SET id = 0;
+  -- SET email = '';
+  -- SET name = '';
+  -- SET country ='US';
+
+
+  CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
     country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
     );
-
+END $$
+DELIMITER ;
+CALL create_detail;
