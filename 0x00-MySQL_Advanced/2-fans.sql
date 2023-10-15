002b5f6b4662c770893ea29ee9f26dda391aebdf
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-10-15 00:53:06 +0000

    added task 2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -5,8 +5,9 @@ DELIMITER $$
 DROP PROCEDURE IF EXISTS order_band$$
 CREATE PROCEDURE order_band()
 BEGIN
-  SELECT origin, fans as nb_fans FROM metal_bands
-  ORDER BY fans DESC;
+  SELECT origin, SUM(fans) as nb_fans FROM metal_bands
+  GROUP BY origin
+  ORDER BY nb_fans DESC;
 END$$
 DELIMITER ;
 CALL order_band;
