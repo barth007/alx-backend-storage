a50e4f1b42172af70c7c26a30327055cad30c63d
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-10-15 00:46:23 +0000

    added task 2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -2,9 +2,10 @@
 -- number of non unique fans
 
 DELIMITER $$
+DROP PROCEDURE IF EXISTS order_band$$
 CREATE PROCEDURE order_band()
 BEGIN
-  SELECT origin, fans FROM metal_bands
+  SELECT origin, fans as nb_fans FROM metal_bands
   ORDER BY fans DESC;
 END$$
 DELIMITER ;
