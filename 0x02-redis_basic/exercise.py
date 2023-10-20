5ae96106d54701f5d17431382e6b488fe4a19d3d
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-10-20 12:51:00 +0000

    task 1 done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """
 exercise.py
 """
-from typing import Union
+from typing import Union, Optional, Callable
 import redis
 import uuid
 
@@ -14,7 +14,7 @@ class Cache:
     """
 
     def __init__(self, host='localhost', port=6379, db=0):
-        self._redis = redis.Redis()
+        self._redis = redis.Redis(host=host, port=port, db=db)
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
@@ -26,3 +26,31 @@ class Cache:
         random_key_str: str = str(random_key)
         self._redis.set(random_key_str, data)
         return random_key_str
+
+    def get(self, key: str,
+            *fn: Optional[Callable[[str], None]]) -> Union[str, bytes, int]:
+        """
+        This is a typed annoted method
+        """
+
+        if not self._redis.exists(key):
+            return None
+        value = self._redis.get(key)
+        if fn is not None:
+            return fn(value)
+        else:
+            return value
+
+    def get_int(self, key: str) -> int:
+        """
+        This is type annoted method that returns an integer
+        """
+
+        return self.get(key, int)
+
+    def get_str(self, key: str) -> str:
+        """
+        type annoted method that returns an string
+        """
+
+        return self.get(key, str)
